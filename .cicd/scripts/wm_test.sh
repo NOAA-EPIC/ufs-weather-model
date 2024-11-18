1e1c3cff8455c251876ef3df626c1ed3d3e7bbec
Bruce Kropp <bruce.kropp@raytheon.com>
2024-11-18 15:22:46 -0600

    add wm_test script
    Signed-off-by: Bruce Kropp <bruce.kropp@raytheon.com>

--- .cicd/scripts/wm_test.sh
+++ .cicd/scripts/wm_test.sh
@@ -14,6 +14,131 @@ export FC=${FC:-mpif90}
 BUILD_DIR=${BUILD_DIR:-${UFS_MODEL_DIR}/build}
 TESTS_DIR=${TESTS_DIR:-${UFS_MODEL_DIR}/tests}
 
-cd "${TESTS_DIR}"
+(
+	cd "${TESTS_DIR}"
+	pwd
+	ls -al ./rt.sh
+)
+
 pwd
-ls -al ./rt.sh
+echo "NODE_NAME=${NODE_NAME}"
+echo "USER=${USER}"
+echo "UFS_PLATFORM=<${UFS_PLATFORM}>"
+echo "UFS_COMPILER=<${UFS_COMPILER}>"
+echo "WM_REGRESSION_TESTS=<${WM_REGRESSION_TESTS}>"
+echo "WM_OPERATIONAL_TESTS=<${WM_OPERATIONAL_TESTS}>"
+export workspace=$(pwd)
+machine=${NODE_NAME}
+echo "machine=<${machine}>"
+machine_id=${UFS_PLATFORM}
+if [[ ${UFS_PLATFORM} =~ clusternoaa ]] ; then
+	machine_id="noaacloud"
+	#sed -i -e "s|EPIC/spack-stack/spack-stack-1.5.0|spack-stack/spack-stack-1.5.1|g" modulefiles/ufs_noaacloud.intel.lua
+fi
+echo "machine_id=<${machine_id}>"
+
+status=0
+
+export LMOD_SH_DBG_ON=0
+echo "LMOD_VERSION=${LMOD_VERSION}"
+
+/usr/bin/time -p \
+	-o ${WORKSPACE}/${UFS_PLATFORM}-${UFS_COMPILER}-time-wm_test.json \
+	-f '{\n  "cpu": "%P"\n, "memMax": "%M"\n, "mem": {"text": "%X", "data": "%D", "swaps": "%W", "context": "%c", "waits": "%w"}\n, "pagefaults": {"major": "%F", "minor": "%R"}\n, "filesystem": {"inputs": "%I", "outputs": "%O"}\n, "time": {"real": "%e", "user": "%U", "sys": "%S"}\n}' \
+ls -l build/ufs_model
+status=$?
+
+#[[ ${UFS_PLATFORM} == jet         ]] && WM_REGRESSION_TESTS=false   # takes too long
+#[[ ${UFS_PLATFORM} == derecho     ]] && WM_REGRESSION_TESTS=false
+#[[ ${UFS_PLATFORM} =~ clusternoaa ]] && WM_REGRESSION_TESTS=false
+
+rm -f ${workspace}/wm_test_results-${UFS_PLATFORM}-${UFS_COMPILER}.txt
+if [[ ${WM_REGRESSION_TESTS} = true ]] ; then
+
+	set +x
+	if [[ ${UFS_PLATFORM} = orion ]] ; then
+		git --version
+		#module --ignore_cache load git/2.28.0
+		git submodule update --init --recursive
+	fi
+
+	if [[ ${UFS_PLATFORM} = gaea ]] ; then
+		source /gpfs/f5/epic/scratch/role.epic/contrib/Lmod_init_C5.sh
+		echo "LMOD_VERSION=${LMOD_VERSION}"
+	fi
+
+	module use $PWD/modulefiles >/dev/null 2>&1
+	module load ufs_${machine_id}.${UFS_COMPILER} || true
+	[[ ${UFS_PLATFORM} = gaea ]] && module load cmake/3.23.1
+	module list
+	set -x
+
+	#export PATH=$PATH:~/bin
+	echo "CHANGE_ID=$CHANGE_ID"
+
+	export FV3_RT_DIR=
+	#export JENKINS_WORKSPACE=
+	workflow="-e" # -e = ecflow (default) || -r = rocoto
+
+	#export ACCNR=epic
+	echo "ACCNR=${ACCNR}"
+
+	opt="-l"
+	suite="rt.conf"
+	[[ -n ${WM_OPERATIONAL_TESTS}                 ]] && opt="-n" && suite="${WM_OPERATIONAL_TESTS} ${UFS_COMPILER}"
+	[[    ${WM_OPERATIONAL_TESTS} = default       ]] && opt="-n" && suite="control_p8 ${UFS_COMPILER}"
+	[[    ${WM_OPERATIONAL_TESTS} = comprehensive ]] && opt="-l" && suite="rt.conf"
+	[[    ${WM_OPERATIONAL_TESTS} = rt.conf       ]] && opt="-l" && suite="rt.conf"
+	[[ ${suite} = rt.conf ]] && opt="-l"
+
+	[[ ${WM_CREATE_BASELINE} = true   ]] && export BL_DATE=$(cat tests/bl_date.conf | cut -d '=' -f2)
+	[[ ! -f tests/logs/RegressionTests_${UFS_PLATFORM,,}.log ]] || mv tests/logs/RegressionTests_${UFS_PLATFORM,,}.log tests/logs/RegressionTests_${UFS_PLATFORM,,}.log.orig
+
+	rm -f ${workspace}/${UFS_PLATFORM}-${UFS_COMPILER}-wm_*-log.txt
+	if [[ ${WM_CREATE_BASELINE} = true ]] ; then
+		echo "start Creating baseline on ${UFS_PLATFORM} ..."
+		ls -al .cicd/*
+		echo "Pipeline Creating Baseline Tests ${suite} on ${UFS_PLATFORM} ${UFS_COMPILER}: (${opt} [${suite:=rt.conf}])"
+		/usr/bin/time -p \
+			-o ${WORKSPACE}/${UFS_PLATFORM}-${UFS_COMPILER}-time-wm_test.json \
+			-f '{\n  "cpu": "%P"\n, "memMax": "%M"\n, "mem": {"text": "%X", "data": "%D", "swaps": "%W", "context": "%c", "waits": "%w"}\n, "pagefaults": {"major": "%F", "minor": "%R"}\n, "filesystem": {"inputs": "%I", "outputs": "%O"}\n, "time": {"real": "%e", "user": "%U", "sys": "%S"}\n}' \
+			./.cicd/scripts/create_baseline.sh ${account} ${baseline} ${workflow} -k ${opt} "${suite}" | tee -a ${workspace}/${UFS_PLATFORM}-${UFS_COMPILER}-wm_test-log.txt
+		status=${PIPESTATUS[0]}
+		echo "Pipeline Completed Baseline Tests ${opt} ${suite} on ${UFS_PLATFORM} ${UFS_COMPILER}. status=$status"
+	else
+		echo "skip Creating baseline on ${UFS_PLATFORM}."
+		ls -al .cicd/*
+		echo "Pipeline Running Regression Tests ${suite} on ${UFS_PLATFORM} ${UFS_COMPILER}: (${opt} [${suite:=rt.conf}])"
+		/usr/bin/time -p \
+			-o ${WORKSPACE}/${UFS_PLATFORM}-${UFS_COMPILER}-time-wm_test.json \
+			-f '{\n  "cpu": "%P"\n, "memMax": "%M"\n, "mem": {"text": "%X", "data": "%D", "swaps": "%W", "context": "%c", "waits": "%w"}\n, "pagefaults": {"major": "%F", "minor": "%R"}\n, "filesystem": {"inputs": "%I", "outputs": "%O"}\n, "time": {"real": "%e", "user": "%U", "sys": "%S"}\n}' \
+			./.cicd/scripts/regression_test.sh ${account} ${baseline} ${workflow} -k ${opt} "${suite}" | tee -a ${workspace}/${UFS_PLATFORM}-${UFS_COMPILER}-wm_test-log.txt
+		status=${PIPESTATUS[0]}
+		echo "Pipeline Completed Regression Tests ${opt} ${suite} on ${UFS_PLATFORM} ${UFS_COMPILER}. status=$status"
+	fi
+
+	cd tests/
+		cd logs/
+		#cp RegressionTests_${machine_id}.log ${JENKINS_WORKSPACE}
+		git remote -v
+		git fetch --no-recurse-submodules origin
+		git reset FETCH_HEAD --hard
+		cd .. #&& cd .. && cd ..
+
+		pwd
+		ls -al .
+		ls -al $WORKSPACE/${machine_id}/tests/logs/.
+
+		ls -al logs/.
+		## Test Results ...
+		echo "ExperimentName: ${suite}" | tee -a ${workspace}/${UFS_PLATFORM}-${UFS_COMPILER}-wm_test-log.txt | tee    ${workspace}/wm_test_results-${UFS_PLATFORM}-${UFS_COMPILER}.txt
+		egrep " DIRECTORY: |Time: | Completed: |Result: " logs/RegressionTests_${UFS_PLATFORM,,}.log          | tee -a ${workspace}/wm_test_results-${UFS_PLATFORM}-${UFS_COMPILER}.txt
+		egrep " -- COMPILE | -- TEST "                    logs/RegressionTests_${UFS_PLATFORM,,}.log          | tee -a ${workspace}/wm_test_results-${UFS_PLATFORM}-${UFS_COMPILER}.txt
+	cd ${workspace}
+else
+	echo "Pipeline skipping Regression Tests on ${UFS_PLATFORM} (${machine})"
+	echo "ExperimentName: null" > ${workspace}/wm_test_results-${UFS_PLATFORM}-${UFS_COMPILER}.txt
+fi
+find ${workspace}/tests/logs -ls
+echo "Pipeline Tests on ${UFS_PLATFORM} complete. status=$status"
+exit $status
