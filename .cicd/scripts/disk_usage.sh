5c6903b5801608afe6cfd7497ada695a2644a8d1
Bruce Kropp <bruce.kropp@raytheon.com>
2024-12-10 15:02:30 -0600

    disk_usage outfile
    Signed-off-by: Bruce Kropp <bruce.kropp@raytheon.com>

--- .cicd/scripts/disk_usage.sh
+++ .cicd/scripts/disk_usage.sh
@@ -15,20 +15,23 @@ export UFS_COMPILER=${UFS_COMPILER:-intel}
 [[ -n ${WORKSPACE} ]] || WORKSPACE="$(pwd)"
 [[ -n ${UFS_PLATFORM} ]] || UFS_PLATFORM="$(hostname -s 2>/dev/null)" || UFS_PLATFORM="$(hostname 2>/dev/null)"
 [[ -n ${UFS_COMPILER} ]] || UFS_COMPILER="compiler"
+echo "STAGE_NAME=${STAGE_NAME%% *}" # from pipeline
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+echo "script_dir=${script_dir}"
 
 # Get repository root from Jenkins WORKSPACE variable if set, otherwise, set
 # relative to script directory.
 declare workspace
 if [[ -d "${WORKSPACE}/${UFS_PLATFORM}" ]]; then
     workspace="${WORKSPACE}/${UFS_PLATFORM}"
+    outfile="${4:-${workspace}-${UFS_PLATFORM}-${UFS_COMPILER}-disk-usage${STAGE_NAME%% *}.csv}"
 else
     workspace="$(cd -- "${script_dir}/../.." && pwd)"
+    outfile="${4:-${workspace}/${UFS_PLATFORM}-${UFS_COMPILER}-disk-usage${STAGE_NAME%% *}.csv}"
 fi
-
-echo "STAGE_NAME=${STAGE_NAME%% *}" # from pipeline
-outfile="${4:-${workspace}-${UFS_COMPILER}-disk-usage${STAGE_NAME%% *}.csv}"
+echo "workspace=${workspace}"
+echo "outfile=${outfile}"
 
 function disk_usage() {
     local directory="${1:-${PWD}}"
