fa667b693f0815100b7b92e59f9833bc2c99c271
Bruce Kropp <bruce.kropp@raytheon.com>
2024-12-10 13:56:10 -0600

    create an init stage time file
    Signed-off-by: Bruce Kropp <bruce.kropp@raytheon.com>

--- .cicd/scripts/wm_init.sh
+++ .cicd/scripts/wm_init.sh
@@ -17,6 +17,24 @@ BUILD_DIR=${BUILD_DIR:-${UFS_MODEL_DIR}/build}
 TESTS_DIR=${TESTS_DIR:-${UFS_MODEL_DIR}/tests}
 
 cd "${UFS_MODEL_DIR}"
-pwd
 echo "UFS_PLATFORM=<${UFS_PLATFORM}>"
 echo "UFS_COMPILER=<${UFS_COMPILER}>"
+
+pwd
+echo "NODE_NAME=${NODE_NAME}"
+echo "UFS_PLATFORM=${UFS_PLATFORM}"
+echo "UFS_COMPILER=${UFS_COMPILER}"
+workspace=$(pwd)
+export workspace
+machine=${NODE_NAME}
+echo "machine=<${machine}>"
+machine_id=${UFS_PLATFORM,,}
+if [[ ${UFS_PLATFORM} =~ clusternoaa ]] ; then
+	machine_id="noaacloud"
+fi
+echo "machine_id=<${machine_id}>"
+
+/usr/bin/time -p \
+	-o ${workspace}/${UFS_PLATFORM}-${UFS_COMPILER}-time-wm_init.json \
+	-f '{\n  "cpu": "%P"\n, "memMax": "%M"\n, "mem": {"text": "%X", "data": "%D", "swaps": "%W", "context": "%c", "waits": "%w"}\n, "pagefaults": {"major": "%F", "minor": "%R"}\n, "filesystem": {"inputs": "%I", "outputs": "%O"}\n, "time": {"real": "%e", "user": "%U", "sys": "%S"}\n}' \
+	pwd
