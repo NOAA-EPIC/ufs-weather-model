5bfec58cf9e0d5de7592963ae092075be0184a3b
root <root@ip-10-29-93-229.ec2.internal>
2025-08-19 21:13:03 +0000

    Fixes.

--- tests-dev/ufs_test_utils.sh
+++ tests-dev/ufs_test_utils.sh
@@ -3,6 +3,9 @@ set -eux
 
 if [[ ${MACHINE_ID} == "ursa" ]]; then
     PYTHON_VER="python3"
+elif [[ ${MACHINE_ID} == "ami" ]]; then
+    PYTHON_VER="python3"
+    export PATH="/usr/bin:$PATH"
 else
     PYTHON_VER="python"
 fi
