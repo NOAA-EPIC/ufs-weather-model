8e9f6ed514c7de0bd619301a0bab30ffbad3204b
Gillian Petro <gillian.petro@noaa.gov>
2025-09-23 13:52:10 -0400

    comment out extra accept in header

--- .github/scripts/parse_historical_data.py
+++ .github/scripts/parse_historical_data.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
       "Accept": "application/vnd.github.v3+json",
       "Authorization": "Bearer {token}",
       "X-GitHub-Api-Version": "2022-11-28",
-      "Accept": "application/vnd.github.raw"
+      #"Accept": "application/vnd.github.raw"
    }
    token = os.environ.get('GITHUB_TOKEN')
    machines = os.environ.get('MACHINES').split()
