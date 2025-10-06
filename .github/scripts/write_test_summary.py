f45c1be8b400a41ae6663c2c20e05b6120b4cecf
Gillian Petro <gillian.petro@noaa.gov>
2025-10-06 11:30:23 -0400

    refactor write-to-file functionality

--- .github/scripts/write_test_summary.py
+++ .github/scripts/write_test_summary.py
@@ -2,10 +2,11 @@ import os
 import json
 import re
 from mdutils.mdutils import MdUtils
+import pandas as pd
 
-def load_json():
+def load_json(file_path):
    """Convert JSON file to python dictionary."""
-   with open(os.environ.get('RESULTS'), 'r', encoding='utf-8') as file:
+   with open(file_path, 'r', encoding='utf-8') as file:
       data = json.load(file)
 
    return data
@@ -16,60 +17,60 @@ def create_mdFile():
 
    return mdFile
 
-def build_runtime_content(contents, mdFile):
-   table_content = []#"Tests", "Runtime"]
-   fails = 0
-
-   for test in contents:  
-      if contents[test][0].startswith("❌ FAIL"):
-         table_content.append(test)
-         #table_content.append(contents[test][0])
-         fails += 1
-
-   return table_content, fails
-
-def build_memory_content(contents, mdFile):
-   table_content = []#"Tests", "Memory"]
-   fails = 0
-   
-   for test in contents:  
-      if contents[test][1].startswith("❌ FAIL"):
-         table_content.append(test)
-         #table_content.append(contents[test][1])
-         fails += 1
-
-   return table_content, fails
-
-def build_content(contents, mdFile, machine):
-
-   rt_results, rt_fails = build_runtime_content(contents, mdFile)
-   mem_results, mem_fails = build_memory_content(contents, mdFile)
-   
-   # Create a table
-   mdFile.write(f"<details><summary>{machine.upper()} - Runtime/Memory Summary</summary>")
-   mdFile.new_paragraph('\n')
-   mdFile.new_paragraph('\n')
-   mdFile.write(f"<h4>RUNTIME: {(len(contents) - rt_fails)} / {len(contents)} had normal runtime.</h4>")
-   mdFile.new_paragraph(f"Tests with anomolously high runtime: ")
-   mdFile.new_list(items=rt_results, marked_with='*')
-   #mdFile.new_table(columns=2, rows=rt_fails+1, text_align='center', text=rt_results)
-   mdFile.new_paragraph('\n')
-   mdFile.new_paragraph('\n')
-   mdFile.write(f"<h4>MEMORY: {(len(contents) - mem_fails)} / {len(contents)} had normal memory usage.</h4>")
-   mdFile.new_paragraph(f"Tests with anomolously high memory usage: ")
-   mdFile.new_list(items=mem_results, marked_with='*')
-   #mdFile.new_table(columns=2, rows=mem_fails+1, text_align='center', text=mem_results)
-   mdFile.write('</details>')
-   mdFile.new_paragraph('\n')
-
-
-if __name__ == "__main__":
+def build_content(category):
 
    machines = os.environ.get('MACHINES').split()
-   results = os.environ.get('RESULTS')
-   contents = load_json()
-   file = create_mdFile()
+   print(f"{category.upper()}_RESULTS")
+   contents = load_json(os.environ.get(f"{category.upper()}_RESULTS"))
+   results = pd.DataFrame()
+   
    for machine in machines:
-      build_content(contents[machine], file, machine)
 
-   print(file.get_md_text())
+      machine_results = pd.DataFrame.from_dict(contents[machine], orient='index',columns=[machine])
+      results = pd.merge(results, machine_results, left_index=True, right_index=True, how='outer').fillna("N/A")
+
+   return results
+
+def write_content(data, mdFile):
+   
+   machines = os.environ.get('MACHINES').split()
+   
+   # Build contents list starting with header row
+   contents = ["Test"] + machines
+
+   # Create table
+   mdFile.write(f"<details><summary>Runtime Summary</summary>")
+   mdFile.new_paragraph('\n')
+
+   for index, row in data.iterrows():
+
+      contents.append(str(index))
+      for item in row:   
+         contents.append(item)
+   
+   mdFile.new_table(columns=(len(machines) + 1), rows=(len(data) + 1), text_align='center', text=contents)
+   mdFile.new_paragraph('\n')
+   mdFile.write('</details>')
+
+   return mdFile
+
+def create_summary():
+   
+   categories = ['RUNTIME', 'MEM']
+
+   mdFile = create_mdFile()
+
+   for category in categories: 
+      data = build_content(category)
+      mdFile = write_content(data, mdFile)
+   
+   return mdFile
+   
+def main():
+
+   summary = create_summary()
+   print(summary.get_md_text())
+
+if __name__ == "__main__":
+   
+   main()
