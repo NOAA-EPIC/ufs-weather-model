9f6a0b44971a1c90878a16beb6e16966f4183707
Gillian Petro <gillian.petro@noaa.gov>
2025-10-08 14:14:36 -0400

    change comparison to HEAD of PR instead of HEAD of develop

--- .github/scripts/get_data.py
+++ .github/scripts/get_data.py
@@ -27,48 +27,73 @@ class Log():
    def __init__(self, machine):
       """Initialize the log file with a corresponding machine."""
       self.machine = machine
+      self.text_per_log = []
+
+   def call_API(self, endpoint):
+
+      api_call = APICall(endpoint)
+      response = requests.get(api_call.url, headers=api_call.header)
+      response = json.loads(response.text)
+
+      return response
 
    # Update number of commits to 50 before merging
-   def get_commits(self, num_commits=10):
-      """Get a list of commits for the log, with a maximum of 100.
-      Structure of response: 
-      response = [
-      {"sha":"#######...",
+   def get_repo_commits(self, num_commits=1):
+      """Get a list of commits for the log, with a maximum of 100 and a default of 1. Structure of response: 
+      response = [{
+      "sha":"#######...",
       "node_id":"C_...",
       "commit":{
          "author":{
             "name":"First Last",
             "email":"########+username@users.noreply.github.com",
             "date":"YYYY-MM-DDTHH:mm:SSZ"
-         },
-      ...
-      ]
-   """
+         }, ... ]
+      """
       endpoint = f"commits?path=tests/logs/RegressionTests_{self.machine}.log&per_page={num_commits}"
-      api_call = APICall(endpoint)
-      response = requests.get(api_call.url, headers=api_call.header)
-      response = json.loads(response.text)
-      self.commits = []
+      response = self.call_API(endpoint)
+      
+      self.repo_commits = []
       for num in range(len(response)): 
          if response[num]['sha']:
-            self.commits.append(response[num]['sha'])
+            self.repo_commits.append(response[num]['sha'])
          else: 
             print(response[num]['sha'], "does not exist!")
 
-   def get_log_text(self): 
+
+   def get_pr_head(self):
+      """Get SHA for the HEAD of the PR. Structure of response: 
+         response = [{
+            "head": {
+               "label": "user:feature-branch",
+               "ref": "feature-branch",
+               "sha": "a1b2c3d..."
+            // ...
+            }, ...
+         }]
+      """
+      endpoint = f"pulls/{os.environ.get('PR_NUM')}"
+      response = self.call_API(endpoint)
+      self.pr_head_commit = [response['head']['sha']]
+      print(self.pr_head_commit)
+
+
+   def get_log_text(self, commits): 
       """For each commit of a log, extract the log text."""
 
-      if not self.commits:
+      if not commits:
          return "ERROR: This log has no commit list. Log text cannot be extracted. "
       
-      self.log_text = []
-      endpoint = f"contents/tests/logs/RegressionTests_{self.machine}.log"
-      api_call = APICall(endpoint)
+      api_call = APICall(f"contents/tests/logs/RegressionTests_{self.machine}.log")
       
-      for num in range(len(self.commits)): 
-         url = api_call.url + (f"?ref={self.commits[num]}") #Could use a path join?
+      for num in range(len(commits)): 
+         url = api_call.url + (f"?ref={commits[num]}") #Could use a path join?
          r = requests.get(url, headers=api_call.header)
-         self.log_text.append(r.text)
+         if commits == self.pr_head_commit:
+            # Ensure that the pr log text comes first
+            self.text_per_log.insert(0,r.text)
+         else:
+            self.text_per_log.append(r.text)
 
    def get_instance_test_data(self, log_instance):
       """For each instance of a log at a given commit, extract runtime and memory data from the log text
@@ -89,7 +114,7 @@ class Log():
             test_name, hh, mm, mem = test_match.groups()
             total_minutes = int(hh) * 60 + int(mm)
             tests_for_log_instance[test_name] = [total_minutes, int(mem)]
-      
+
       return tests_for_log_instance     
       
    def get_historical_log_data(self): # Could split for runtime, mem to make more maintainable
@@ -104,7 +129,8 @@ class Log():
    
       self.historical_rt_mem_data = {}
       
-      for log_instance in self.log_text:
+      # Skip self.text_per_log[0] because it is the log from the PR
+      for log_instance in self.text_per_log[1:]:
          
          data = self.get_instance_test_data(log_instance)
          for test in data:
@@ -125,9 +151,17 @@ class Log():
          memory_stdev = round(np.std(self.historical_rt_mem_data[test]["memory"]), 5)
          self.test_stats[test] = [runtime_mean, runtime_stdev, memory_mean, memory_stdev]
 
+   def get_current_pr_data(self):
+
+      self.get_pr_head()
+      self.get_log_text(self.pr_head_commit)
+      pr_log_data = self.get_instance_test_data(self.text_per_log[0])
+      
+      return pr_log_data
+
    def gather_historical_data(self):
-      self.get_commits()
-      self.get_log_text()
+      self.get_repo_commits(num_commits=10) #increase for statistical significance
+      self.get_log_text(self.repo_commits)
       self.get_historical_log_data()
       self.calculate_stats()
 
@@ -136,35 +170,44 @@ class Log():
       self.runtime_results = {}
 
       for test in current_log:
-         hi_rt = self.test_stats[test][0] + self.test_stats[test][1]
-         if current_log[test][0] > hi_rt and previous_logs['last'][test][0] > hi_rt and previous_logs['second_to_last'][test][0] > hi_rt:
-            self.runtime_results[test] = f"❌"
-         elif current_log[test][0] > hi_rt:
-            self.runtime_results[test] = f"⚠️"
-         else:
-            self.runtime_results[test] = '✅'
+
+         try:
+            hi_rt = self.test_stats[test][0] + self.test_stats[test][1]
+            if current_log[test][0] > hi_rt and previous_logs['last'][test][0] > hi_rt and previous_logs['second_to_last'][test][0] > hi_rt:
+               self.runtime_results[test] = '❌'
+            elif current_log[test][0] > hi_rt:
+               self.runtime_results[test] = '⚠️'
+            else:
+               self.runtime_results[test] = '✅'
+         except KeyError:
+            print(test, "is new. No comparison data.")
+            self.runtime_results[test] = 'New'
+
 
    def compare_memory(self, current_log, previous_logs):
 
       self.memory_results = {}
 
       for test in current_log:
-         hi_mem = self.test_stats[test][2] + self.test_stats[test][3]
-         if current_log[test][0] > hi_mem and previous_logs['last'][test][0] > hi_mem and previous_logs['second_to_last'][test][0] > hi_mem:
-            self.memory_results[test] = f"❌"
-         elif current_log[test][0] > hi_mem:
-            self.memory_results[test] = f"⚠️"
-         else:
-            self.memory_results[test] = '✅'
+         try:
+            hi_mem = self.test_stats[test][2] + self.test_stats[test][3]
+            if current_log[test][0] > hi_mem and previous_logs['last'][test][0] > hi_mem and previous_logs['second_to_last'][test][0] > hi_mem:
+               self.memory_results[test] = '❌'
+            elif current_log[test][0] > hi_mem:
+               self.memory_results[test] = '⚠️'
+            else:
+               self.memory_results[test] = '✅'
+         except KeyError:
+            print(test, "is new. No comparison data.")
+            self.runtime_results[test] = 'New'
 
    def compare_results(self): 
    
-      current_log = self.get_instance_test_data(self.log_text[0])
+      current_log = self.get_instance_test_data(self.text_per_log[0])
       previous_logs = {"last" : {}, "second_to_last" : {}}
 
-
       for index, item in enumerate(previous_logs):
-         previous_logs[item] = self.get_instance_test_data(self.log_text[index + 1])
+         previous_logs[item] = self.get_instance_test_data(self.text_per_log[index + 1])
       
       self.compare_runtime(current_log, previous_logs)
       self.compare_memory(current_log, previous_logs)
@@ -189,6 +232,7 @@ def main():
    for machine in machines:
       print(machine.upper())
       log = Log(machine)
+      log.get_current_pr_data()
       log.gather_historical_data()
       log.compare_results()
       stats_by_machine[machine] = log.test_stats
