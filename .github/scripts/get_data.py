77879b6c410b94593aa1488133b5bbbdd1c2be0d
Gillian Petro <gillian.petro@noaa.gov>
2025-10-10 14:13:21 -0400

    refactoring, caching

--- .github/scripts/get_data.py
+++ .github/scripts/get_data.py
@@ -6,7 +6,7 @@ import re
 import numpy as np
 
 class APICall():
-   """An API call"""
+   """An GitHub API call"""
 
    def __init__(self, endpoint='', num_commits=1):
       self.token = os.environ.get('GITHUB_TOKEN')
@@ -25,11 +25,12 @@ class Log():
    """A Regression Test log file."""
    
    def __init__(self, machine):
-      """Initialize the log file with a corresponding machine."""
+      """Create the log file object for a specific machine."""
       self.machine = machine
       self.text_per_log = []
 
    def call_API(self, endpoint):
+      """Call the GitHub API to get information about the log file"""
 
       api_call = APICall(endpoint)
       response = requests.get(api_call.url, headers=api_call.header)
@@ -38,8 +39,8 @@ class Log():
       return response
 
    # Update number of commits to 50 before merging
-   def get_repo_commits(self, num_commits=1):
-      """Get a list of commits for the log, with a maximum of 100 and a default of 1. Structure of response: 
+   def fetch_repo_commits(self, num_commits=1):
+      """Get a list of commits for the log file, with a maximum of 100 and a default of 1. Structure of response: 
       response = [{
       "sha":"#######...",
       "node_id":"C_...",
@@ -47,11 +48,11 @@ class Log():
          "author":{
             "name":"First Last",
             "email":"########+username@users.noreply.github.com",
-            "date":"YYYY-MM-DDTHH:mm:SSZ"
+            "date":"YYYY-MM-DDTHH:mm:SSZ",
+         ...
          }, ... ]
       """
-      endpoint = f"commits?path=tests/logs/RegressionTests_{self.machine}.log&per_page={num_commits}"
-      response = self.call_API(endpoint)
+      response = self.call_API(f"commits?path=tests/logs/RegressionTests_{self.machine}.log&per_page={num_commits}")
       
       self.repo_commits = []
       for num in range(len(response)): 
@@ -72,13 +73,12 @@ class Log():
             }, ...
          }]
       """
-      endpoint = f"pulls/{os.environ.get('PR_NUM')}"
-      response = self.call_API(endpoint)
+      response = self.call_API(f"pulls/{os.environ.get('PR_NUM')}")
       self.pr_head_commit = [response['head']['sha']]
       print(self.pr_head_commit)
 
 
-   def get_log_text(self, commits): 
+   def fetch_log_text(self, commits): 
       """For each commit of a log, extract the log text."""
 
       if not commits:
@@ -117,7 +117,7 @@ class Log():
 
       return tests_for_log_instance     
       
-   def get_historical_log_data(self): # Could split for runtime, mem to make more maintainable
+   def compile_historical_log_data(self): # Could split for runtime, mem to make more maintainable
       """Create a dictionary of data with runtime and memory usage for each test over time. Structure:  
          historical_test_data = {
             test: {
@@ -133,6 +133,7 @@ class Log():
       for log_instance in self.text_per_log[1:]:
          
          data = self.get_instance_test_data(log_instance)
+         #print(data)
          for test in data:
             try: 
                self.historical_rt_mem_data[test]["runtime"].append(data[test][0])
@@ -140,6 +141,7 @@ class Log():
             except KeyError: 
                # Create key if it doesn't exist yet
                self.historical_rt_mem_data[test] = {"runtime": [data[test][0]], "memory": [data[test][1]]}
+      print(self.historical_rt_mem_data)
                
    def calculate_stats(self):
       """For each test, calculate the mean and standard deviation of memory and runtime."""
@@ -150,20 +152,6 @@ class Log():
          memory_mean = round(np.mean(self.historical_rt_mem_data[test]["memory"]), 5)
          memory_stdev = round(np.std(self.historical_rt_mem_data[test]["memory"]), 5)
          self.test_stats[test] = [runtime_mean, runtime_stdev, memory_mean, memory_stdev]
-
-   def get_current_pr_data(self):
-
-      self.get_pr_head()
-      self.get_log_text(self.pr_head_commit)
-      pr_log_data = self.get_instance_test_data(self.text_per_log[0])
-      
-      return pr_log_data
-
-   def gather_historical_data(self):
-      self.get_repo_commits(num_commits=10) #increase for statistical significance
-      self.get_log_text(self.repo_commits)
-      self.get_historical_log_data()
-      self.calculate_stats()
 
    def compare_runtime(self, current_log, previous_logs):
       
@@ -212,6 +200,21 @@ class Log():
       self.compare_runtime(current_log, previous_logs)
       self.compare_memory(current_log, previous_logs)
 
+   def get_current_pr_data(self):
+
+      self.get_pr_head()
+      self.fetch_log_text(self.pr_head_commit)
+      pr_log_data = self.get_instance_test_data(self.text_per_log[0])
+      
+      return pr_log_data
+
+   def gather_historical_data(self):
+      self.fetch_repo_commits(num_commits=10) #increase for statistical significance
+      self.fetch_log_text(self.repo_commits)
+      self.compile_historical_log_data()
+      self.calculate_stats()
+
+
 def create_machine_stats(stats_dict):
    """Create a json file with statistic for each test on each machine"""
 
@@ -224,16 +227,35 @@ def create_machine_results(results_dict, file_name):
    with open(f"data/{file_name}.json", 'a') as fh:
       json.dump(results_dict, fh, indent=4)
 
+def load_json(file_path):
+   """Convert JSON file to python dictionary."""
+   with open(file_path, 'r', encoding='utf-8') as file:
+      data = json.load(file)
+
+   return data
+
 def main():
+   """For each machine, create a log object, get current PR data, gather historical runtime/memory data, 
+   and compare results to determine which test/machine combinations fall more than 2 standard deviations 
+   above the historical mean for each test."""
+
    machines = os.environ.get('MACHINES').split()
+   
+   # Contains mean and standard deviation for each test on each machine
    stats_by_machine = {}
+   # Contains information on whether test runtime was more than 2 standard deviations above the mean. 
    runtime_results_by_machine = {}
+   # Contains information on whether test memory was more than 2 standard deviations above the mean. 
    mem_results_by_machine = {}
-   for machine in machines:
+
+   for machine in machines[:1]:
       print(machine.upper())
-      log = Log(machine)
+      log = Log(machine.lower())
       log.get_current_pr_data()
-      log.gather_historical_data()
+      if os.environ.get('TEST_STATS'):
+         log.test_stats = load_json(os.environ.get('TEST_STATS'))
+      else:
+         log.gather_historical_data()
       log.compare_results()
       stats_by_machine[machine] = log.test_stats
       runtime_results_by_machine[machine] = log.runtime_results
