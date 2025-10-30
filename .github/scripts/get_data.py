b5ce1e1eceaddd7a2aa6d0fde371c87fe4566236
Gillian Petro <gillian.petro@noaa.gov>
2025-10-30 13:17:22 -0400

    fix mem_results bug, privatize methods, consolidate file I/O

--- .github/scripts/get_data.py
+++ .github/scripts/get_data.py
@@ -39,9 +39,17 @@ class Log():
 
       return response
 
+   def _get_pr_head(self):
+      """Get SHA for the HEAD of the PR. Structure of response: 
+         response = [{"head": {"sha": "a1b2c3d..."}}]
+         See GitHub documentation for https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-commits
+      """
+      response = self.call_API(f"pulls/{os.environ.get('PR_NUM')}")
+      self.pr_head_commit = [response['head']['sha']]
+
    # Update number of commits to 50 before merging
-   def fetch_repo_commits(self, num_commits=1):
-      """Get a list of commits for the log file, with a maximum of 100 and a default of 1. 
+   def _fetch_repo_commits(self, num_commits=1):
+      """Get a list of commits for the log file from the authoritative repository, with a maximum of 100 and a default of 1. 
       Structure of response: response = [{'sha': '3jl26ka...'}, {'sha': '6ag43sb...'}, ...]
       See GitHub documentation for https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-commits
       """
@@ -54,35 +62,24 @@ class Log():
          else: 
             logging.error(f"{response[num]['sha']} does not exist!")
 
-   def get_pr_head(self):
-      """Get SHA for the HEAD of the PR. Structure of response: 
-         response = [{"head": {"sha": "a1b2c3d..."}}]
-         See GitHub documentation for https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-commits
-      """
-      response = self.call_API(f"pulls/{os.environ.get('PR_NUM')}")
-      self.pr_head_commit = [response['head']['sha']]
-
-   def fetch_log_text(self, commits): 
+   def _fetch_log_text(self, commits): 
       """For each commit of a log, extract the log text."""
 
-      if not commits:
-         return "ERROR: This log has no commit list. Log text cannot be extracted. "
-      
-      if not self.pr_head_commit:
-         return "ERROR: Must call get_pr_head() before this function! "
+      try:
+         api_call = APICall(f"contents/tests/logs/RegressionTests_{self.machine}.log")
+         
+         for num in range(len(commits)): 
+            url = api_call.url + (f"?ref={commits[num]}") #Could use a path join?
+            r = requests.get(url, headers=api_call.header)
+            if commits == self.pr_head_commit:
+               # Ensure that the pr log text comes first
+               self.text_per_log.insert(0,r.text)
+            else:
+               self.text_per_log.append(r.text)
+      except AttributeError():
+         logging.error("An appropriate commit(s) was not provided. Call _get_pr_head() or _fetch_repo_commits() first.")
 
-      api_call = APICall(f"contents/tests/logs/RegressionTests_{self.machine}.log")
-      
-      for num in range(len(commits)): 
-         url = api_call.url + (f"?ref={commits[num]}") #Could use a path join?
-         r = requests.get(url, headers=api_call.header)
-         if commits == self.pr_head_commit:
-            # Ensure that the pr log text comes first
-            self.text_per_log.insert(0,r.text)
-         else:
-            self.text_per_log.append(r.text)
-
-   def get_instance_test_data(self, log_instance):
+   def _get_instance_test_data(self, log_instance):
       """For each instance of a log at a given commit, extract runtime and memory data from the log text
          Args:
             log_instance: Log text for a given commit
@@ -104,7 +101,7 @@ class Log():
 
       return tests_for_log_instance     
       
-   def compile_historical_log_data(self): # Could split for runtime, mem to make more maintainable
+   def _compile_historical_log_data(self): # Could split for runtime, mem to make more maintainable
       """Create a dictionary of data with runtime and memory usage for each test over time. Structure:  
          historical_test_data = {
             test: {
@@ -119,7 +116,7 @@ class Log():
       # Skip self.text_per_log[0] because it is the log from the PR
       for log_instance in self.text_per_log[1:]:
          
-         data = self.get_instance_test_data(log_instance)
+         data = self._get_instance_test_data(log_instance)
          for test in data:
             try: 
                self.historical_rt_mem_data[test]["runtime"].append(data[test][0])
@@ -158,8 +155,6 @@ class Log():
             logging.info(f"{test} is new. No comparison data.")
             self.runtime_results[test] = 'New'
 
-      print(self.runtime_results)
-
    def _compare_memory(self, current_log, previous_logs):
       """Determine whether the test memory usage is within normal bounds."""
 
@@ -181,11 +176,11 @@ class Log():
    def compare_results(self): 
       """Check results from previous two commits to determine whether the test runtime/memory usage is within normal bounds."""
 
-      current_log = self.get_instance_test_data(self.text_per_log[0])
+      current_log = self._get_instance_test_data(self.text_per_log[0])
       previous_logs = {"last" : {}, "second_to_last" : {}}
 
       for index, item in enumerate(previous_logs):
-         previous_logs[item] = self.get_instance_test_data(self.text_per_log[index + 1])
+         previous_logs[item] = self._get_instance_test_data(self.text_per_log[index + 1])
       
       self._compare_runtime(current_log, previous_logs)
       self._compare_memory(current_log, previous_logs)
@@ -193,29 +188,25 @@ class Log():
    def get_current_pr_data(self):
       """Extract runtime/memory data for the PR's most recent commit."""
 
-      self.get_pr_head()
-      self.fetch_log_text(self.pr_head_commit)
-      pr_log_data = self.get_instance_test_data(self.text_per_log[0])
+      self._get_pr_head()
+      self._fetch_log_text(self.pr_head_commit)
+      pr_log_data = self._get_instance_test_data(self.text_per_log[0])
       
       return pr_log_data
 
    def gather_historical_data(self, num_commits=2):
       """Extract runtime/memory data for the authoritative repository's last two commits."""
-      self.fetch_repo_commits(num_commits) #increase for statistical significance
-      self.fetch_log_text(self.repo_commits)
-      self.compile_historical_log_data()
+      self._fetch_repo_commits(num_commits) #increase for statistical significance
+      self._fetch_log_text(self.repo_commits)
+      self._compile_historical_log_data()
 
-def create_machine_stats(stats_dict):
-   """Create a json file with statistic for each test on each machine"""
+"""Utilities for file I/O"""
 
-   with open(f"data/stats.json", 'w') as fh:
-      json.dump(stats_dict, fh, indent=4)
-
-def create_machine_results(results_dict, file_name):
-   """Create a json file with statistic for each test on each machine"""
+def create_json(dictionary, file_name):
+   """Create a json file with statistics for each test on each machine"""
 
    with open(f"data/{file_name}.json", 'w') as fh:
-      json.dump(results_dict, fh, indent=4)
+      json.dump(dictionary, fh, indent=4)
 
 def load_json(file_path):
    """Convert JSON file to python dictionary."""
@@ -251,7 +242,7 @@ def main():
       else:
          log.gather_historical_data(10) # past 10 commits
          log.calculate_stats()
-         stats_by_machine[machine] = log.test_stats
+         stats_by_machine[machine] = log.test_stats # Add stats to save/cache later
 
       # Compare and save results
       log.compare_results()
@@ -260,11 +251,11 @@ def main():
    
    # If the statistics on mean/standard deviation have NOT already been cached, create file to cache.
    if not os.environ.get('TEST_STATS'):
-      create_machine_stats(stats_by_machine)
+      create_json(stats_by_machine, "stats")
 
    # Create resource summaries to use in write_test_summary.py 
-   create_machine_results(runtime_results_by_machine, "runtime_results")
-   create_machine_results(mem_results_by_machine, "memory_results")
+   create_json(runtime_results_by_machine, "runtime_results")
+   create_json(mem_results_by_machine, "memory_results")
 
 if __name__ == "__main__":
 
