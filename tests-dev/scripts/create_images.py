29890e44d95616604bc2f553c62f5d37ae9b550e
Gillian Petro <gillian.petro@noaa.gov>
2025-12-19 14:49:11 -0500

    modify hash direction for plots

--- tests-dev/scripts/create_images.py
+++ tests-dev/scripts/create_images.py
@@ -72,6 +72,8 @@ def plot_results(data, category):
    metrics = organize_data_by_test(data, category)
    hashes = get_hashes(10) # Change to 50
    hashes.insert(0, "PR Head")
+   hashes.reverse()
+   #print(hashes)
 
    # Create one plot per test
    for test in metrics:
@@ -80,12 +82,11 @@ def plot_results(data, category):
       styles = ['o-', 's--', '^-', 'd:', 'x-.', 'v--', '*-', 'p:']
 
       plt.title(f"{category} for {test}", fontsize=16)
-      plt.xlabel("Commit Hash", fontsize=14)
+      plt.xlabel("Commit Hash: oldest --> newest", fontsize=14)
       plt.ylabel(category, fontsize=14)
       plt.xticks(np.arange(len(hashes)), labels=hashes, rotation=45, fontsize=10)
       plt.yticks(fontsize=12)
       plt.grid(True, linestyle='--', alpha=0.5)
-      
 
       # Add one line to the plot with data for each machine
       for i, machine in enumerate(metrics[test]):
@@ -96,7 +97,7 @@ def plot_results(data, category):
          # so take the most recent hashes for which there is data
          x = hashes[:len(y)]
          # Plot lines per machine, then add anomalies for each line
-         plt.plot(x, y, styles[i % len(styles)], label=f"{machine}", linewidth=2, markersize=6)
+         plt.plot(x, y[::-1], styles[i % len(styles)], label=f"{machine}", linewidth=2, markersize=6)
          [plt.plot(x[idx], y[idx], 'ro', markersize=8) for idx, val in enumerate(anomalies) if val == True]
          
       plt.legend(fontsize=12)
