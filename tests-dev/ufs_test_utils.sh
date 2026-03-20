1a053113f16cbf07533e4ad29291e29f8b1a7228
Gillian Petro <gillian.petro@noaa.gov>
2026-03-19 22:45:02 -0400

    fix warnings in tests-dev; update checkout action

--- tests-dev/ufs_test_utils.sh
+++ tests-dev/ufs_test_utils.sh
@@ -10,6 +10,7 @@ fi
 function set_run_task() {
     source default_vars.sh
     source rt_utils.sh
+    PATHRT=${PATHRT:-}
     source "${PATHRT}"/tests/"${TEST_NAME}"
     #jkim compute_petbounds_and_tasks
     if [[ ${ESMF_THREADING} == true ]]; then
