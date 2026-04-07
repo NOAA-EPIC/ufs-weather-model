4b122547028ee62dc4568cc251405252c34746dd
Gillian Petro <gillian.petro@noaa.gov>
2026-04-07 16:57:28 -0400

    check for failures, too

--- .github/scripts/check_log_warnings_remarks.py
+++ .github/scripts/check_log_warnings_remarks.py
@@ -35,8 +35,7 @@ class Log():
       api_call = APICall(endpoint)
       response = requests.get(api_call.url, headers=api_call.header)
       if response.status_code != 200:
-         logging.warning(response)
-         print(response)
+         logging.error(f"{response}: API call failed for {api_call.url}")
          sys.exit(1)
       response = json.loads(response.text)
       
@@ -61,51 +60,59 @@ class Log():
          r = requests.get(url, headers=api_call.header)
          return r.text
       except:
-         logging.error("An appropriate commit(s) was not provided. Call _get_commits() first.")
-
-   def _get_test_data(self, log_instance):
-      """For each instance of a log at a given commit, extract runtime and memory data from the log text
-         Args:
-            log_instance: Log text for a given commit
-         Returns: 
-            tests_for_log_instance: A dictionary of tests (keys) with a tuple of warnings and remarks as the value for each test
-      """
-
-      tests_for_log_instance = {}
-
-      # Use non-capturing groups in pattern to indicate warnings/remarks may or may not be present.
-      compile_pattern = r"COMPILE \'(.*)\' \[\d+:\d+, \d+:\d+\](?: \( (?:(\d+) warnings)?\s*(?:(\d+) remarks)? \))?"
-      failure_pattern = r"^(?:FAILED|SKIPPED): (?!UNABLE TO (?:COMPLETE COMPARISON|START TEST))(.+?) -- (?:TEST|COMPILE) '([^']+)"
-
-
-      log_instance = log_instance.splitlines()
-
-      for line in log_instance:
-         test_match_compile = re.search(compile_pattern, line)
-         failure_match = re.search(failure_pattern, line)
-         if test_match_compile:
-            test_name, warnings, remarks = test_match_compile.groups()
-            tests_for_log_instance[test_name] = (warnings, remarks)
-         if failure_match:
-            # Add logic
-            print(failure_match)
-            pass
-
-      return tests_for_log_instance
-
-   def _get_pr_data(self, commit):
-      """Extract warnings/remarks data for a particular commit.
-      Returns:
-         log_data: A dictionary of tests as the key with a tuple of (warnings, remarks) as the value
-      """
-      try:
-         log_text = self._fetch_log_text(commit)
-         log_data = self._get_test_data(log_text)
-         log_data = self._clean_data(log_data)
-         return log_data
-      except:
          logging.error(f"No commit found for the ref {commit}")
          sys.exit(1)
+
+   def _get_pr_warn_rmk(self, log_text):
+      """Extract warnings/remarks data for a particular commit.
+      Returns:
+         log_warn_rmk: A dictionary of tests as the key with a tuple of (warnings, remarks) as the value
+      """
+      
+      log_warn_rmk = {}
+      
+      # Use non-capturing groups in pattern to indicate warnings/remarks may or may not be present.
+      compile_pattern = r"COMPILE \'(.*)\' \[\d+:\d+, \d+:\d+\](?: \( (?:(\d+) warnings)?\s*(?:(\d+) remarks)? \))?"
+
+      log_text = log_text.splitlines()
+
+      for line in log_text:
+         test_match_compile = re.search(compile_pattern, line)
+         if test_match_compile:
+            test_name, warnings, remarks = test_match_compile.groups()
+            log_warn_rmk[test_name] = (warnings, remarks)
+         
+      log_warn_rmk = self._clean_data(log_warn_rmk)
+      
+      return log_warn_rmk
+
+   def _get_failures(self, log_text):
+      """For each instance of a log at a given commit, list of failed or skipped tests and compiles
+         Args:
+            log_text: Log text for a given commit
+         Returns: 
+            failures: A dictionary of tests (keys) with a tuple of warnings and remarks as the value for each test
+      """
+
+      failures = {}
+
+      # Use non-capturing groups in pattern to indicate warnings/remarks may or may not be present.
+      failure_pattern = r"^(?:FAILED|SKIPPED): (?!UNABLE TO (?:COMPLETE COMPARISON|START TEST))(.+?) -- (?:TEST|COMPILE) '([^']+)"
+      
+      log_text = log_text.splitlines()
+
+      for line in log_text:
+         failure_match = re.search(failure_pattern, line)
+         if failure_match:
+            reason, test = failure_match.groups()
+            #print(test, reason)
+            try:
+               failures[reason].append(test)
+            except KeyError:
+               failures[reason] = [test]
+         
+      return failures
+
 
    def _clean_data(self, test_data):
       """Convert None values to zeros in the test_data dictionary"""
@@ -133,11 +140,11 @@ class Log():
       
       return increases
 
-def print_html_results(dict):
-   """Print the comparison results in HTML."""
+def print_warn_rmk_results(dict):
+   """Print the results from the warnings/remarks comparison in HTML."""
    
    pr_num = os.environ.get('PR_NUM')
-   mdFile = MdUtils(file_name='summary.md', title=f'Increased Warnings/Remarks for PR #{pr_num}')
+   mdFile = MdUtils(file_name='warn_rmk.md', title=f'Increased Warnings/Remarks for PR #{pr_num}')
 
    for machine, results in dict.items():
       for category in results.keys():
@@ -149,6 +156,21 @@ def print_html_results(dict):
             mdFile.new_list(unordered_list, marked_with='*')
    return mdFile.get_md_text()
 
+def print_failure_results(dict):
+   """Print the results from the warnings/remarks comparison in HTML."""
+   
+   pr_num = os.environ.get('PR_NUM')
+   mdFile = MdUtils(file_name='failures.md', title=f'Compile and Test Failures for PR #{pr_num}')
+
+   for machine, failures in dict.items():
+      for reason, tests in failures.items():
+         mdFile.write(f"\n<h3>{machine.upper()}</h3>\n")
+         unordered_list = [f"**{reason.upper()}:**", []]
+         for test in tests:
+            unordered_list[1].append(f"{test}")
+         mdFile.new_list(unordered_list, marked_with='*')
+   return mdFile.get_md_text()
+
 def main():
    """For each machine, create a log object, get current PR data, and determine 
    which tests increase warnings and/or remarks on each machine.""" 
@@ -157,21 +179,30 @@ def main():
 
    # For each machine, tests where warnings and/or remarks increase
    increased_warnings_remarks = {}
+   failures = {}
 
    for machine in machines:
       log = Log(machine)
       log._get_commits()
-      log.pr_log_data = log._get_pr_data(log.pr_head_commit)
-      log.base_log_data = log._get_pr_data(log.pr_base_commit)
+      log.pr_log_text = log._fetch_log_text(log.pr_head_commit)
+      log.pr_warn_rmk = log._get_pr_warn_rmk(log.pr_log_text)
+      log.pr_failures = log._get_failures(log.pr_log_text)
+
+      log.base_log_text = log._fetch_log_text(log.pr_base_commit)
+      log.base_warn_rmk = log._get_pr_warn_rmk(log.base_log_text)
       
-      increased_warnings_remarks[machine] = log.compare_results(log.pr_log_data, log.base_log_data)
+      increased_warnings_remarks[machine] = log.compare_results(log.pr_warn_rmk, log.base_warn_rmk)
+      failures[machine] = log.pr_failures
 
-   results = print_html_results(increased_warnings_remarks)
+   warn_rmk_results = print_warn_rmk_results(increased_warnings_remarks)
+   failure_results = print_failure_results(failures)
 
-   if len(results) > 81: # Length of HTML header
-      print(results)
+   if len(warn_rmk_results) > 81: # Length of HTML header
+      print(warn_rmk_results)
+      print(failure_results)
       sys.exit(1)
    else:
+      print(failure_results)
       sys.exit(0)
 
 if __name__ == "__main__": # pragma: no coverage
